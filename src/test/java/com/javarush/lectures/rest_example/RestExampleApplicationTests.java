c83ac30c81f98a123a2725c8f0ecd0991b52ad41
Evgeny Rudoy <rudoy95@mail.ru>
2021-01-07 02:17:49 +0300

    Initial commit

---
+++ src/test/java/com/javarush/lectures/rest_example/RestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarush.lectures.rest_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
